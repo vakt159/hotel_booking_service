95f77984b5b4f5e0992572c998c368b191d43241
vadym-zahrebelnyi <zahrebelnyivadym@gmail.com>
2026-01-14 02:39:41 +0200

    feat: modify db environment preparation

--- hotel_booking_service/settings.py
+++ hotel_booking_service/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = "hotel_booking_service.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
