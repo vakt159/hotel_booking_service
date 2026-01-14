4b25e64f47a4186f721b6e46251b1542a7a1086d
Artem <artemp2208@gmail.com>
2026-01-14 13:13:24 +0200

    Create Room view

--- room/views.py
+++ room/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+from room.models import Room
+from room.serializers import RoomSerializer
+from room.permissions import IsAdminOrReadOnly
+
+
+class RoomViewSet(ModelViewSet):
+    queryset = Room.objects.all().order_by("id")
+    serializer_class = RoomSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ("type", "capacity")
