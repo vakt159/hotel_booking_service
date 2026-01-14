08456cb1ce7dcf174f0df01aba54e3bd8005ac63
Andrii Yermolenko <vakt159@gmail.com>
2026-01-14 12:23:39 +0200

    Add JWT authorization

--- hotel_booking_service/settings.py
+++ hotel_booking_service/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    'rest_framework_simplejwt',
     "guest",
     "payment",
     "room",
@@ -122,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
+}
