137cf4ff2063d5ec8e7b3227a24f8649ddd4c88f
dmytro-malyk-dm <dmytro.malyk7@gmail.com>
2026-01-15 10:11:01 +0200

    feat: add Booking model to admin panel

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from booking.models import Booking
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "room",
+        "user",
+        "check_in_date",
+        "check_out_date",
+        "actual_check_out_date",
+        "status",
+        "price_per_night",
+    )
+
+    list_filter = (
+        "status",
+        "check_in_date",
+        "check_out_date",
+        "room",
+    )
+
+    search_fields = (
+        "user__email",
+        "room__number",
+    )
+
+    ordering = ("-check_in_date",)
