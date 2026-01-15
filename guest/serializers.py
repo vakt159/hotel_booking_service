fdb172d3b72b7e5b9e571e4668e4d40412d666a2
Andrii Yermolenko <vakt159@gmail.com>
2026-01-15 18:22:40 +0200

    Add first_name, last_name to registration and change style of password entering to be hidden

--- guest/serializers.py
+++ guest/serializers.py
@@ -1,13 +1,22 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
+from django.utils.translation import gettext_lazy as _
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "password", "is_staff")
+        fields = ("id", "email", "password", "first_name", "last_name",
+                  "is_staff")
         read_only_fields = ("is_staff",)
-        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+        extra_kwargs = {"password":
+            {
+                "write_only": True,
+                "min_length": 5,
+                "style": {"input_type": "password"},
+                "label": _("Password")
+            }}
 
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
