e9f430e24ea3b9ace358f30395a820c0cbee15cf
VYGAKOOL <vygakool@gmail.com>
2026-01-16 20:48:26 +0200

    Add new endpoints

--- payment/urls.py
+++ payment/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from payment.views import TestStripeSessionView
-
+from payment.views import StripeWebhook, PaymentSuccessView, PaymentCancelView, \
+    PaymentListView
 
 app_name = "payments"
 
 urlpatterns = [
-    path("test-session/", TestStripeSessionView.as_view(), name="test-session"),
+    path("", PaymentListView.as_view(), name="payment-list"),
+    path("webhook/", StripeWebhook.as_view(), name="stripe-webhook"),
+    path("success/", PaymentSuccessView.as_view(), name="success"),
+    path("cancel/", PaymentCancelView.as_view(), name="cancel")
 ]
