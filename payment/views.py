a7b6e1b132c4fda96943682d4ca478cfb6c2b08a
VYGAKOOL <vygakool@gmail.com>
2026-01-16 20:52:49 +0200

    Add ApiView for payments list, stripe webhook, cancel-success endpoints

--- payment/views.py
+++ payment/views.py
@@ -1,30 +1,72 @@
 import stripe
 from django.conf import settings
+from rest_framework import status, generics
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 from payment.models import Payment
-from booking.models import Booking
-from payment.stripe_helper import create_checkout_session
+from payment.serializers import PaymentSerializer
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
-class TestStripeSessionView(APIView):
-    def post(self, request):
-        booking = Booking.objects.first()  # тимчасово
+class PaymentListView(generics.ListAPIView):
+    queryset = Payment.objects.all().order_by("-id")
+    serializer_class = PaymentSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = [IsAuthenticated]
 
-        payment = Payment.objects.create(
-            booking=booking,
-            type=Payment.PaymentType.BOOKING,
-            money_to_pay=booking.price_per_night
-        )
 
-        session_url = create_checkout_session(
-            payment,
-            success_url="http://localhost:8000/success",
-            cancel_url="http://localhost:8000/cancel",
-        )
+class StripeWebhook(APIView):
+    authentication_classes = []
+    permission_classes = []
 
-        return Response({
-            "session_url": session_url
-        })
+    def post(self, request, *args, **kwargs):
+        payload = request.body
+        sig_header = request.META.get("HTTP_STRIPE_SIGNATURE")
+        event = None
+        payment = None
+
+        try:
+            event = stripe.Webhook.construct_event(
+                payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+            )
+        except stripe.error.SignatureVerificationError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        if event["type"] != "checkout.session.completed":
+            return Response(status=status.HTTP_200_OK)
+        elif event["type"] == "checkout.session.completed":
+            session = event["data"]["object"]
+            session_id = session["id"]
+
+            try:
+                payment = Payment.objects.get(session_id=session_id)
+            except Payment.DoesNotExist:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+
+        if payment.status != Payment.PaymentStatus.PAID:
+            payment.status = Payment.PaymentStatus.PAID
+            payment.save(update_fields=["status"])
+
+        return Response(status=status.HTTP_200_OK)
+
+
+class PaymentSuccessView(APIView):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"detail": "Payment successful!"})
+
+
+class PaymentCancelView(APIView):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"detail": "Payment cancelled"})
