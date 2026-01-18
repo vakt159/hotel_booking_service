3ef3d9e8aa07c6c6ec17afeacfd8de04390ce2e3
dmytro-malyk-dm <dmytro.malyk7@gmail.com>
2026-01-18 19:16:53 +0200

    feat: add functionality for expire_stripe_sessions which marks payments as EXPIRED

--- payment/tasks.py
+++ payment/tasks.py
@@ -1,5 +1,8 @@
 from celery import shared_task
+from django.utils import timezone
+from datetime import timedelta
 
+from payment.models import Payment
 from booking.models import Booking
 from payment.services.payment_service import create_booking_payment
 
@@ -18,3 +21,25 @@ def create_stripe_payment_task(booking_id, payment_type):
     return (
         f"Payment {payment.id} of type {payment_type} created for Booking {booking_id}"
     )
+
+
+@shared_task
+def expire_stripe_sessions():
+    """
+    Fallback task:
+    Marks payments as EXPIRED if Stripe session
+    was created more than 24 hours ago
+    and payment is still PENDING.
+    """
+    expiration_threshold = timezone.now() - timedelta(minutes=1)
+
+    expired_payments = Payment.objects.filter(
+        status=Payment.PaymentStatus.PENDING,
+        created_at__lte=expiration_threshold,
+    )
+
+    count = expired_payments.update(
+        status=Payment.PaymentStatus.EXPIRED
+    )
+
+    return f"Expired {count} payments older than 24 hours"
